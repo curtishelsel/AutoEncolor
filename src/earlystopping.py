16fe3c06380fd8b23841a4a981474fadc7ab0b56
Curtis Helsel <curtisdhelsel@gmail.com>
2021-12-01 21:15:10 -0500

    Add earlystopping for training

---
+++ src/earlystopping.py
@@ -0,0 +1,35 @@
+import numpy as np
+import torch
+
+class EarlyStopping:
+
+    def __init__(self, patience=0, threshold=1e-4):
+
+        self.patience = patience
+        self.epoch_count = 0
+        self.best_loss = np.Inf 
+        self.threshold = threshold
+        self.stop = False
+
+    def __call__(self, current_loss):
+    
+        loss_threshold = self.best_loss * (1 - self.threshold)
+
+        if current_loss < loss_threshold:
+            self.epoch_count = 0
+            self.best = current_loss
+        else:
+            self.epoch_count += 1
+
+        if self.epoch_count > self.patience:
+            self.stop = True
+            print('Maximum epoch threshold exeeded')
+            print('Training stopping early')
+
+
+        
+
+
+
+
+
