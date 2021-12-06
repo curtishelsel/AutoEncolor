e70417c3114581fb95163409513b17dce8583696
Curtis Helsel <curtisdhelsel@gmail.com>
2021-12-05 19:26:01 -0500

    Add final comments and restructure code

--- src/earlystopping.py
+++ src/earlystopping.py
@@ -1,3 +1,8 @@
+# This file contains the class for early stopping
+# during training based on validation loss
+# Author: Curtis Helsel
+# December 2021
+
 import torch
 import numpy as np
 
@@ -11,10 +16,19 @@ class EarlyStopping:
         self.threshold = threshold
         self.stop = False
 
+    # Based on the current loss of the training
+    # and the patience value set, set the stop variable
     def __call__(self, current_loss):
     
+        # This threshold forumla was taken from pytorch's 
+        # reduce on learning rate plataeu function
+        # and helps to limit how often the threshold is met
         loss_threshold = float(self.best_loss) * (1 - self.threshold)
 
+        # If the current loss is less than threshold
+        # then restart the counter and set the current
+        # best loss to current loss otherwise, 
+        # increment the counter 
         if current_loss < loss_threshold:
             if self.epoch_count != 0:
                 print('Early stopping epoch count reset.')
@@ -23,7 +37,9 @@ class EarlyStopping:
         else:
             self.epoch_count += 1
             print('Early stopping {} of {}'.format(self.epoch_count, self.patience))
-
+    
+        # If the counter is over the patience level
+        # set the stop variable for early stopping
         if self.epoch_count > self.patience:
             self.stop = True
             print('Maximum epoch threshold exeeded.')
