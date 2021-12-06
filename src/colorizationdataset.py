21a0b0e9701fe2214fe283ef762b698283f1fd26
Curtis Helsel <curtisdhelsel@gmail.com>
2021-12-05 19:11:29 -0500

    Add final comments

--- src/colorizationdataset.py
+++ src/colorizationdataset.py
@@ -1,11 +1,21 @@
+# This file contains the class for the 
+# custom dataset for loading images to use
+# in training and with DataLoader
 from torchvision import datasets, transforms
 
+# This class loads images in a subfolder,
+# and converts them from RGB to grayscale
 class ColorizationDataset(datasets.ImageFolder):
     
+    # Gets individual image and transforms
+    # image to grayscale, returns original
+    # image as target and grayscale input
     def __getitem__(self, index):
         path, _ = self.imgs[index]
         img = self.loader(path)
 
+        # Transforms the image to grayscale
+        # and make it a tensor
         self.transforms = transforms.Compose([
                             transforms.Grayscale(),
                             transforms.ToTensor()
